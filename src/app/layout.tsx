629bfce397c2b31decfb1e61273a2ede6b98009d
Florian Hacqueville-Mathi <florian.hacqueville@hotmail.fr>
2024-08-23 17:48:14 +0200

    cleaning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import type { Viewport } from 'next'
-import SanFrancisco from 'next/font/local';
 import Popps from 'next/font/local'
 import './globals.css';
 import { Provider } from "react-redux";
@@ -11,9 +10,7 @@ import Footer from './components/Footer/Footer';
 import { DataProvider } from './contexts/data';
 import { cn } from './lib/utils';
  
-const SF = SanFrancisco({ src: "./assets/fonts/Poppins/Poppins-Regular.ttf"});
-const Poppins = Popps({ src: "./assets/fonts/San Francisco/pro/SF-Pro-Display-Regular.otf"})
-
+const Poppins = Popps({ src: "./assets/fonts/Poppins/Poppins-Regular.ttf"})
 
 export const viewport: Viewport = {
   themeColor: 'black',
