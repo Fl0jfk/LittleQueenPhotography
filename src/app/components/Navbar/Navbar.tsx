5a9b848d84852d0079e481739ab4514842025757
Florian Hacqueville-Mathi <florian.hacqueville@hotmail.fr>
2024-08-31 11:20:13 +0200

    socials

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -87,14 +87,14 @@ export default function Navbar({menuOpened, onLinkClick} :NavbarProps ){
                             <button className='hover:scale-105 text-center text-white' onClick={()=>handleModalOpen('Grossesse')} aria-label="Lien vers la page portrait émotionnel">Grossesse</button>                           
                         </div>
                         <div className='flex gap-4'>
-                        <Link href="/" className="hover:scale-[1.1]" onClick={handleLinkClick} target="blank">
+                        <Link href="https://www.instagram.com/littlequeenphotography/?hl=fr" className="hover:scale-[1.1]" onClick={handleLinkClick} target="blank">
                                 <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none">
                                     <path fillRule="evenodd" clipRule="evenodd" d="M12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z" fill="#FFF"/>
                                     <path d="M18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z" fill="#FFF"/>
                                     <path fillRule="evenodd" clipRule="evenodd" d="M1.65396 4.27606C1 5.55953 1 7.23969 1 10.6V13.4C1 16.7603 1 18.4405 1.65396 19.7239C2.2292 20.8529 3.14708 21.7708 4.27606 22.346C5.55953 23 7.23969 23 10.6 23H13.4C16.7603 23 18.4405 23 19.7239 22.346C20.8529 21.7708 21.7708 20.8529 22.346 19.7239C23 18.4405 23 16.7603 23 13.4V10.6C23 7.23969 23 5.55953 22.346 4.27606C21.7708 3.14708 20.8529 2.2292 19.7239 1.65396C18.4405 1 16.7603 1 13.4 1H10.6C7.23969 1 5.55953 1 4.27606 1.65396C3.14708 2.2292 2.2292 3.14708 1.65396 4.27606ZM13.4 3H10.6C8.88684 3 7.72225 3.00156 6.82208 3.0751C5.94524 3.14674 5.49684 3.27659 5.18404 3.43597C4.43139 3.81947 3.81947 4.43139 3.43597 5.18404C3.27659 5.49684 3.14674 5.94524 3.0751 6.82208C3.00156 7.72225 3 8.88684 3 10.6V13.4C3 15.1132 3.00156 16.2777 3.0751 17.1779C3.14674 18.0548 3.27659 18.5032 3.43597 18.816C3.81947 19.5686 4.43139 20.1805 5.18404 20.564C5.49684 20.7234 5.94524 20.8533 6.82208 20.9249C7.72225 20.9984 8.88684 21 10.6 21H13.4C15.1132 21 16.2777 20.9984 17.1779 20.9249C18.0548 20.8533 18.5032 20.7234 18.816 20.564C19.5686 20.1805 20.1805 19.5686 20.564 18.816C20.7234 18.5032 20.8533 18.0548 20.9249 17.1779C20.9984 16.2777 21 15.1132 21 13.4V10.6C21 8.88684 20.9984 7.72225 20.9249 6.82208C20.8533 5.94524 20.7234 5.49684 20.564 5.18404C20.1805 4.43139 19.5686 3.81947 18.816 3.43597C18.5032 3.27659 18.0548 3.14674 17.1779 3.0751C16.2777 3.00156 15.1132 3 13.4 3Z" fill="#FFF"/>
                                 </svg>
                             </Link> 
-                            <Link href="/" className="hover:scale-[1.1]" onClick={handleLinkClick} target="blank">
+                            <Link href="https://www.facebook.com/marine.guerrache.96/?paipv=0&eav=Afb2shS1Rw1mEwdD_hor9x99WYPK-0YDtNIAfiMy4sFGa9D_yHWiYrEOhXl0xE7AtTY&_rdr" className="hover:scale-[1.1]" onClick={handleLinkClick} target="blank">
                                 <svg width="25px" height="25px" viewBox="-5 0 20 20">
                                     <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
                                         <g transform="translate(-385.000000, -7399.000000)" fill="#000000">
@@ -110,7 +110,7 @@ export default function Navbar({menuOpened, onLinkClick} :NavbarProps ){
                                     <path d="M20.23 1.604c-0.008-0-0.017-0-0.027-0-5.961 0-10.793 4.832-10.793 10.793s4.832 10.793 10.793 10.793c5.955 0 10.783-4.822 10.793-10.775v-0.001c-0.004-5.953-4.816-10.781-10.763-10.809h-0.003zM1.004 1.604v28.792h5.274v-28.792z"></path>
                                 </svg>
                             </Link>  
-                            <Link href="/" className="hover:scale-[1.1]" onClick={handleLinkClick} target="blank">
+                            <Link href="https://www.linkedin.com/in/marine-guerrache-905b24141/?original_referer=https%3A%2F%2Fwww%2Egoogle%2Ecom%2F&originalSubdomain=fr" className="hover:scale-[1.1]" onClick={handleLinkClick} target="blank">
                                 <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none">
                                     <path d="M6.5 8C7.32843 8 8 7.32843 8 6.5C8 5.67157 7.32843 5 6.5 5C5.67157 5 5 5.67157 5 6.5C5 7.32843 5.67157 8 6.5 8Z" fill="#fff"/>
                                     <path d="M5 10C5 9.44772 5.44772 9 6 9H7C7.55228 9 8 9.44771 8 10V18C8 18.5523 7.55228 19 7 19H6C5.44772 19 5 18.5523 5 18V10Z" fill="#fff"/>
